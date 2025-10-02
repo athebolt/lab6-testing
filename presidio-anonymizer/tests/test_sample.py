da36893446b937bb1ff6c30beec2decffc125862
Alexander Thebolt <143462967+athebolt@users.noreply.github.com>
2025-10-02 18:47:55 +0000

    updated test_sample.py

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -4,9 +4,8 @@ from presidio_anonymizer.sample import sample_run_anonymizer
 def test_sample_run_anonymizer():
     result = sample_run_anonymizer("My name is Bond.", 11, 15)
     
-    assert result.__getattribute__("text") == "My name is BIP."
-    assert result.__getattribute__("items")[0].start == 11
-    assert result.__getattribute__("items")[0].end == 14
-    assert result.__getattribute__("items")[0].entity_type == 'PERSON'
-    assert result.__getattribute__("items")[0].text == 'BIP'
-    assert result.__getattribute__("items")[0].operator == 'replace'
+    assert result.text == "My name is BIP."
+    assert len(result.items) == 1
+    assert result.items[0].start == 11
+    assert result.items[0].end == 14
+    assert result.items[0].text == 'BIP'
